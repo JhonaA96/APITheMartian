0b5f644996eefdfe04c8a4306546c38020480e32
Jhonatan Acuña <jacuna61@ucatolica.edu.co>
2022-10-30 20:21:15 -0500

    Correction with file on swagger param

--- src/main/java/com/parcial/elMarciano/controller/ActorPictureController.java
+++ src/main/java/com/parcial/elMarciano/controller/ActorPictureController.java
@@ -5,23 +5,23 @@ import com.parcial.elMarciano.service.ActorPictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.websocket.server.PathParam;
 
 @RestController
-@RequestMapping(value = "/picture/", headers=("content-type=multipart/form-data"))
+//@RequestMapping(value = "/picture/", headers=("content-type=multipart/form-data"))
 public class ActorPictureController {
 
     @Autowired
     private ActorPictureService actorPictureService;
 
-    @PostMapping("upload")
+    @RequestMapping(
+            value = "/picture/",
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> uploadPic(@RequestParam("file") MultipartFile picture){
         try{
             actorPictureService.uploadPic(picture);
